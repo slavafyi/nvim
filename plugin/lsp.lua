d1804bbc8f993dc68d438aa7a44c34bf5e445199
Slava Maksimov <204412653+slavafyi@users.noreply.github.com>
2025-05-19 23:06:11 +0300

    feat: add lsp keymaps

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -11,6 +11,14 @@ local function on_attach(_, bufnr)
   vim.keymap.set({ 'n', 'x' }, '<Leader>f', function()
     require('conform').format { async = true }
   end, { desc = 'LSP format document', buffer = bufnr })
+  -- stylua: ignore start
+  vim.keymap.set('n', '<Leader>k', vim.lsp.buf.signature_help, { desc = 'LSP display signature information', buffer = bufnr })
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'LSP jump to the definition', buffer = bufnr })
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'LSP jump to the declaration', buffer = bufnr })
+  vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, { desc = 'LSP jump to the definition of the type', buffer = bufnr })
+  vim.keymap.set('n', '<Leader>q', vim.diagnostic.setqflist, { desc = 'Open diagnostic quickfix list' })
+  vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, { desc = 'Open float diagnostic' })
+  -- stylua: ignore end
 end
 
 later(function()
