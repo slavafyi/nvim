c8d6bf671586dd874ded41f97f7cc49558c6e1c1
Slava Maksimov <28267670+slavafyi@users.noreply.github.com>
2025-09-27 22:46:26 +0300

    feat(lsp): add yaml_ls

---
+++ lsp/yaml_ls.lua
@@ -0,0 +1,20 @@
+---@type vim.lsp.Config
+return {
+  cmd = { 'yaml-language-server', '--stdio' },
+  filetypes = {
+    'yaml',
+    'yaml.ansible',
+    'yaml.docker-compose',
+    'yaml.gitlab',
+    'yaml.helm-values',
+  },
+  root_markers = { '.git' },
+  settings = {
+    redhat = { telemetry = { enabled = false } },
+    yaml = { format = { enable = true } },
+  },
+  on_init = function(client)
+    client.server_capabilities.documentFormattingProvider = true
+    client.server_capabilities.documentRangeFormattingProvider = true
+  end,
+}
