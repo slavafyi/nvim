26e3a831c922db79e16b4b72153aa5d8cccf8af2
Slava M. <mailer@noreply.slava.fyi>
2025-12-11 00:33:09 +0300

    feat: add codecompanion plugin

--- plugin/editor.lua
+++ plugin/editor.lua
@@ -151,3 +151,125 @@ later(function()
     },
   }
 end)
+
+later(function()
+  add {
+    source = 'olimorris/codecompanion.nvim',
+    depends = {
+      'nvim-lua/plenary.nvim',
+      'j-hui/fidget.nvim',
+    },
+  }
+
+  local codecompanion = require 'codecompanion'
+
+  codecompanion.setup {
+    adapters = {
+      http = {
+        openai_responses = function()
+          local adapters = require 'codecompanion.adapters'
+
+          ---@type CodeCompanion.HTTPAdapter.OpenAIResponses
+          local config = {
+            schema = {
+              ['reasoning.effort'] = 'none',
+              top_p = {
+                condition = function(self)
+                  local model = self.schema.model.default
+                  if type(model) == 'function' then model = model() end
+                  return not model == 'gpt-5.1-codex-max'
+                end,
+              },
+            },
+          }
+
+          return adapters.extend('openai_responses', config)
+        end,
+      },
+    },
+    strategies = {
+      chat = {
+        adapter = {
+          name = 'openai_responses',
+          model = 'gpt-5.1-codex-max',
+        },
+      },
+      inline = {
+        adapter = {
+          name = 'openai_responses',
+          model = 'gpt-5.1-codex',
+        },
+      },
+      cmd = {
+        adapter = {
+          name = 'openai_responses',
+          model = 'gpt-5.1-codex-mini',
+        },
+      },
+    },
+  }
+
+  codecompanion.register_extension('spinner', {
+    setup = function()
+      local progress = require 'fidget.progress'
+
+      local spinner = {
+        completed = '✓ Completed',
+        error = '✗ Error',
+        cancelled = '- Cancelled',
+      }
+
+      spinner.handles = {}
+
+      ---Format the adapter name and model for display with the spinner
+      ---@param adapter CodeCompanion.ACPAdapter|CodeCompanion.HTTPAdapter
+      ---@return string
+      local function format_adapter(adapter)
+        local parts = {}
+        table.insert(parts, adapter.formatted_name)
+        if adapter.model and adapter.model ~= '' then
+          table.insert(parts, '(' .. adapter.model .. ')')
+        end
+        return table.concat(parts, ' ')
+      end
+
+      local group = vim.api.nvim_create_augroup('codecompanion-spinner', {})
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'CodeCompanionRequestStarted',
+        group = group,
+        callback = function(args)
+          local handle = progress.handle.create {
+            title = '',
+            message = ' Sending...',
+            lsp_client = {
+              name = format_adapter(args.data.adapter),
+            },
+          }
+          spinner.handles[args.data.id] = handle
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'CodeCompanionRequestFinished',
+        group = group,
+        callback = function(args)
+          local handle = spinner.handles[args.data.id]
+          spinner.handles[args.data.id] = nil
+          if handle then
+            if args.data.status == 'success' then
+              handle.message = spinner.completed
+            elseif args.data.status == 'error' then
+              handle.message = spinner.error
+            else
+              handle.message = spinner.cancelled
+            end
+            handle:finish()
+          end
+        end,
+      })
+    end,
+  })
+
+  vim.cmd 'cab cc CodeCompanion'
+end)
